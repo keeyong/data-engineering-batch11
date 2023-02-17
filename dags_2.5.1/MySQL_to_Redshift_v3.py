0fa7221affc3b2ce444fa0f2a8a75f36d0fefaa7
Keeyong (Max) <keeyonghan@hotmail.com>
2023-02-17 15:49:17 -0800

    Update MySQL_to_Redshift_v3.py

--- dags_2.5.1/MySQL_to_Redshift_v3.py
+++ dags_2.5.1/MySQL_to_Redshift_v3.py
@@ -51,7 +51,7 @@ s3_to_redshift_nps = S3ToRedshiftOperator(
     copy_options=['csv', "IGNOREHEADER 1"],
     redshift_conn_id = "redshift_dev_db",
     method = "UPSERT",
-    upsert_keys = ["created_at"],
+    upsert_keys = ["id", "created_at"],
     dag = dag
 )
 
